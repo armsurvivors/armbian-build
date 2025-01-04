9b39ccdb12db8ac7f2a7ee31a2aa6ef73796069f
Ricardo Pardini <ricardo@pardini.net>
2025-01-04 01:21:43 +0100

    moar

--- lib/tools/all-kernels-rewrite-rebase.sh
+++ lib/tools/all-kernels-rewrite-rebase.sh
@@ -9,13 +9,15 @@ cat output/info/kernels.ndjson | jq -r 'to_entries | map("\(.key )=\(.value )") 
 	echo "==> commit_config: $commit_config"
 	echo "==> commit_patches: $commit_patches"
 
-	./compile.sh rewrite-kernel-config $entry
+	./compile.sh rewrite-kernel-config $entry SYNC_CLOCK=no
 	git add .
 	git commit -m "${commit_config}"
 
-	./compile.sh rewrite-kernel-patches $entry
+	./compile.sh rewrite-kernel-patches $entry SYNC_CLOCK=no
 	git add .
 	git commit -m "${commit_patches}"
+
+	rm -rf cache/sources/linux-kernel-worktree
 
 	echo "=> Done kernel: '${kernel}' branch: '${BRANCH}'"
 done
